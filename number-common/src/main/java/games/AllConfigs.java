dabbea52f23dd1ddfa0aefbe7dbbb624fe75f0b0
saman1000 <saman1000@gmail.com>
2023-10-01 12:25:03 -0400

    add powerball 2 * map of game configs * pass game to API, not complete yet

--- number-common/src/main/java/games/AllConfigs.java
+++ number-common/src/main/java/games/AllConfigs.java
@@ -5,14 +5,14 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 @Configuration
 @ConfigurationProperties(prefix = "lottery")
 @Getter
 @Setter
 public class AllConfigs {
 
-    private GameConfig powerball;
-
-    private GameConfig mega;
+    private Map<String, GameConfig> games;
 
 }
--- rest-server/src/main/java/controllers/NumberGeneratorController.java
+++ rest-server/src/main/java/controllers/NumberGeneratorController.java
@@ -3,6 +3,7 @@ package controllers;
 import history.ChanceMethod;
 import history.IGameChanceRecordGenerator;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import restmodels.SetsOfNumbers;
@@ -27,4 +28,19 @@ public record NumberGeneratorController(
 
         return generatedNumberSets;
     }
+
+    @GetMapping("/generate/{game}")
+    public SetsOfNumbers[] generateNumbers(final @PathVariable String game, final @RequestParam int numberOfSets, final @RequestParam ChanceMethod chanceMethod) {
+        SetsOfNumbers[] generatedNumberSets = new SetsOfNumbers[numberOfSets];
+        List<Integer>[] generatedMainNumberSets = megaNumberGeneratorService.generateMainNumbers(chanceMethod, numberOfSets);
+        Integer[] generatedBallNumbers = megaNumberGeneratorService.generateBallNumbers(chanceMethod, numberOfSets);
+
+        for (int counter = 0; counter < numberOfSets; counter++) {
+            generatedNumberSets[counter] =
+                    new SetsOfNumbers(generatedMainNumberSets[counter], generatedBallNumbers[counter]);
+        }
+
+        return generatedNumberSets;
+    }
+
 }
